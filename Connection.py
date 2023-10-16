43e2d1c4521fd789059987b63e800132265e06ab
Rcgil30 <rcgil@uninorte.edu.co>
2023-10-16 14:03:55 -0500

    Conexión a Base de Datos

--- Connection.py
+++ Connection.py
@@ -1,2 +1,30 @@
 import pypyodbc as odbc
 
+DRIVER_NAME = 'SQL SERVER'
+SERVER_NAME = 'LAPTOP-1LCPBRUM'
+DATABASE_NAME = 'Movies'
+
+connection_string = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trusted_Connection=yes;
+    uid=roberto gil garcia;
+    pwd=3012;
+    """
+
+odbc_connection = odbc.connect(connection_string)
+cursor = odbc_connection.cursor()
+
+query = """
+select top 1 *
+from MoviesInfo
+"""
+
+cursor.execute(query)
+
+for row in cursor:
+    print(row)
+
+cursor.close()
+odbc_connection.close()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Laboratorio-Bases-de-Datos
+
+## Proceso de instalación de Base De Datos
+
+Requisito: SQL Server Management Studio
+
+1. Descargar la carpeta de Movies Dataset
+2. Mover la carpeta al directorio C:\
+3. Descargar el archivo Movie DB Install.sql
+4. Ejecutar el archivo dentro de SQL Server Management Studio
+5. Esperar a que se ejecute el query y refrescar las bases de datos
